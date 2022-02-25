d0514875eeab70180f5f3549198aca5be643d099
rymoah <luca.mariot@gmail.com>
2022-02-25 10:05:10 +0100

    Added package and first class for LON analysis

---
+++ src/lon/LocalSearchTools.java
@@ -0,0 +1,12 @@
+package lon;
+
+/**
+ * Class with methods for performing local search/hill climbing on candidate
+ * solutions with fixed Hamming weights.
+ * 
+ */
+public class LocalSearchTools {
+    
+    
+    
+}
